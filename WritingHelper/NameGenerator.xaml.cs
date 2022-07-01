a0ff4bbb106fdcdee3e9432a79b211027e6f387b
Joana <joana.plewnia@hotmail.de>
2022-07-01 20:16:49 +0200

    added ReadMe.md converted some data from different sources by using WritingHelper.csproj

--- WritingHelper/NameGenerator.xaml.cs
+++ WritingHelper/NameGenerator.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -29,7 +30,7 @@ namespace WritingHelper
 
         private List<String> getAllCountries()
         {
-            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper");
+            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper", "firstNames");
             var directories = Directory.GetDirectories(path);
             var countries = getNamesFromPaths(directories.ToList());
             return countries;
@@ -48,7 +49,7 @@ namespace WritingHelper
 
         private List<int> getAllYears(String country)
         {
-            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper", country);
+            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper","firstNames", country);
             var fileNames = Directory.GetFiles(path);
             var years = getYearsFromPaths(fileNames.ToList());
             years.Reverse();
@@ -75,12 +76,47 @@ namespace WritingHelper
             String yob = ((int) dob_combo_box.SelectedItem).ToString();
             String cob = (String) cob_combo_box.SelectedItem;
             String sex = (String) gender_combo_box.SelectedItem;
-            name_label.Content = generateName(yob, cob, sex);
+            name_label.Content = generateName(yob, cob, sex) + " " + generateLastName(yob, cob);
+        }
+
+        private String generateLastName(String yob, String cob)
+        {
+            try
+            {
+                String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    ".FictionWritingHelper", "lastNames", cob, ("yob" + yob + ".txt"));
+                return chooseNameFromList(getLastNamesFromFile(path));
+            }
+            catch (FileNotFoundException f)
+            {
+                if (Int32.Parse(yob) > 1990)
+                {
+                    return generateLastName((Int32.Parse(yob) - 1).ToString(), cob);
+                }
+                else
+                {
+                    //MessageBox.Show("No last Names for this year and country recorded");
+                    return generateLastName((1990).ToString(), cob);
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                if (Int32.Parse(yob) > 1990)
+                {
+                    return generateLastName((Int32.Parse(yob) - 1).ToString(), cob);
+                }
+                else
+                {
+                    return generateLastName((1990).ToString(), cob);
+                }
+            }
+
+            
         }
 
         private String generateName(String yob, String cob, String sex)
         {
-            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper", cob, ("yob" + yob +".txt"));
+            String path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".FictionWritingHelper", "firstNames", cob, ("yob" + yob +".txt"));
             List<String> listOfNamesRightGender = getMatchingNamesFromFile(path, sex);
             return chooseNameFromList(listOfNamesRightGender);
         }
@@ -106,6 +142,23 @@ namespace WritingHelper
             Random rnd = new Random();
             var rand_index = rnd.Next(0, names.Count);
             return names[rand_index];
+        }
+
+        private List<String> getLastNamesFromFile(String path)
+        {
+            List<String> lines = System.IO.File.ReadAllLines(path).ToList();
+            lines.RemoveAt(0);
+            List<String> names = new List<string>();
+            foreach (var line in lines)
+            {
+                var splits = line.Split(',');
+                var name = splits[0].ToLower();
+                var name2 = name.Substring(1);
+                var parsedName = name[0].ToString().ToUpper() + name2;
+                names.Add(parsedName);
+            }
+
+            return names;
         }
     }
     
